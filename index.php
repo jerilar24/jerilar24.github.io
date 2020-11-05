dd9e78c2868971f18a8e40c7d8645a49370df9d3
jerilar24 <36524300+jerilar24@users.noreply.github.com>
2020-11-05 14:24:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <!-- Para incluir codigo de otro archivo se puede usar include o require, pero si el archivo
 agregado tiene error, el require no dejara que el codigo siga, y el include si -->
 <?php include 'templates/header.php'; ?>
-
+codigos
 <?php include 'templates/footer.php'; ?>
 
 </html>
